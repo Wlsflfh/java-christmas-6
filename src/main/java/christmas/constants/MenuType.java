2bc0fda8336a1fbb6d8f39fb409569ec4e22af8c
(Jinriro) <(riro12@hanyang.ac.kr)>
2024-12-09 17:08:04 +0900

    feat(MenuType): 카테고리 별로 메뉴 List화

--- src/main/java/christmas/constants/MenuType.java
+++ src/main/java/christmas/constants/MenuType.java
@@ -1,37 +1,22 @@
 package christmas.constants;
 
+import static christmas.constants.Product.*;
+
+import java.util.List;
+
 public enum MenuType {
     APPETIZER(List.of(MUSHROOM_SOUP, TAPAS, CAESAR_SALAD)),
     MAIN(List.of(T_BONE_STAKE, BBQ_LIBS, SEA_FOOD_PASTA, CHRISTMAS_PASTA)),
-    DRINK(List.of(ZERO_COKE, RED_WINE, CHAMPAGNE)),
-    DESERT(List.of(ICE_CREAM, CHOCOLATE_CAKE));
+    DESSERT(List.of(CHOCOLATE_CAKE, ICE_CREAM)),
+    DRINK(List.of(ZERO_COKE, RED_WINE, CHAMPAGNE));
 
     private final List<Product> products;
 
     MenuType(List<Product> products) {
         this.products = products;
     }
-    ...
-}
-package christmas.domain.vo;
 
-import java.util.Arrays;
-
-public enum Product {
-    MUSHROOM_SOUP("양송이수프", 6_000L),
-    TAPAS("타파스", 5_500L),
-    CAESAR_SALAD("시저샐러드", 8_000L),
-    T_BONE_STAKE("티본스테이크", 55_000L),
-    BBQ_LIBS("바비큐립", 54_000L),
-    SEA_FOOD_PASTA("해산물파스타", 35_000L),
-    ...
-
-    private final String name;
-    private final Long price;
-
-    Product(String name, Long price) {
-        this.name = name;
-        this.price = price;
+    public List<Product> getProducts() {
+        return products;
     }
-    ...
 }
